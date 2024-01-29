d5eeec25db24be361f3d64b2a698f7a143c42bf9
ash <ashpurple9@gmail.com>
2024-01-29 22:24:30 +0900

    [add] Player Name

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Player = ({initialName, symbol}) => {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+    
+    const handleEditClick = () => {
+        setIsEditing(editing => !editing);
+    }
+
+    const handleChange = (event) => {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+          </li>
+    );
+}
+
+export default Player;
