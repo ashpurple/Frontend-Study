541daca967acea7b467d9d62033e30971de77326
ash <ashpurple9@gmail.com>
2024-01-29 22:43:07 +0900

    [add] game board

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName={"Player1"} symbol="O"/>
           <Player initialName={"Player2"} symbol="X"/>
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   )
