5287909c1f0671b741fe193426a2c81dd90f7c4d
ash <ashpurple9@gmail.com>
2024-01-29 22:51:40 +0900

    [add] basic switch

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,15 +1,22 @@
-import Player from "./components/Player"
-import GameBoard from "./components/GameBoard"
+import { useState } from "react";
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  const handleSelectSquare = () => {
+    setActivePlayer(curActivePlayer => curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName={"Player1"} symbol="O"/>
-          <Player initialName={"Player2"} symbol="X"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName={"Player1"} symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName={"Player2"} symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   )
