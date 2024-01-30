4c9796ca1e85b128ceea7a711b86011c60cf5b86
ash <ashpurple9@gmail.com>
2024-01-31 00:04:51 +0900

    [bug-fix] typo

--- 07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,8 +1,12 @@
 const Log = ({ turns }) => {
+    console.log("turns", turns);
+    // console.log(turns.map(turn => {
+    //         console.log(turn.square.row, turn.square.col);
+    //     }));
     return (
         <ol id="log">
             {turns.map(turn => (
-            <li key={`${turn.square.log}${turn.square.col}`}>
+            <li key={`${turn.square.row}${turn.square.col}`}>
                 {turn.player} selected {turn.square.row},{turn.square.col}
             </li>
             ))}
